b0c1e980f8bde2eab84347b8425c2645170c9cce
Александр Николаев <anileks222@gmail.com>
2026-04-05 17:22:06 +0300

    Добавьте файлы проекта.

---
+++ AmusementPark/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmusementPark
+{
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ AmusementPark/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmusementPark
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = ""; // имя пользователя
+        public int Age { get; set; } // возраст пользователя
+    }
+
+}
